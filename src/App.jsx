835b15dfe3ef4b4bd88bd43c886f4a46ab0e1131
devmern2205 <citmern2205@gmail.com>
2024-03-07 12:35:48 +0600

    demo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Menu from "./components/menu/Menu"
+import Banner from "./components/menu/banner/Banner"
+
+
+function App() {
+
+
+  return (
+   <>
+     <Menu/>
+     <Banner/>
+   </>
+  )
+}
+
+export default App
